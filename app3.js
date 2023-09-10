e1c1cd6e95b3fcf13e144559a52950eb2f344ba2
cricket lover <sami76748@gmail.com>
2023-09-09 22:07:13 -0700

    first commit

---
+++ app3.js
@@ -0,0 +1,18 @@
+let = age = 15;
+
+alert("may  " +age+" saal ka ho")
+ 
+let time = 10
+alert(
+    "manay ya jagah "+time+"dafa dakhi hai "
+)
+let year = 2003
+
+document.write("mera janam "+year+" ma hoa")
+ 
+var username ="sami ahmed"
+var product  ="hair color"
+var adress = "mehmoodabad"
+var age = 15
+
+alert(username+product+adress+age)
